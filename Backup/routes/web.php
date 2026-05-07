1f2246ae336e9f0a7fa318a6ed0df0bb5ca9e865
jithoshchandran <jithoshphp@gmail.com>
2026-05-07 23:36:44 +0530

    Unwanted Backup folder removed

--- Backup/routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::redirect('/', '/admin/login');
-
-
-Route::get('/animal/{tag_number}', [App\Http\Controllers\AnimalController::class, 'show'])->name('animal.show');
